e67b63bd443b9b2ef73cbe612350f100e24da53b
vl-ahmadabdul.aziz <vl-ahmadabdul.aziz@cgc.com.my>
2022-11-26 21:34:29 +0800

    style the cart module and add todos section

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,54 @@
+import {Component} from '@angular/core';
+
+interface TodoProp {
+    name: string;
+    checked: boolean;
+}
+
+@Component({
+    selector: 'app-todo-list',
+    templateUrl: './todo-list.component.html',
+    styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+    name = '';
+
+    todos = [
+        {
+            name: 'Go to the store',
+            checked: true,
+        },
+        {
+            name: 'Watch movie',
+            checked: false,
+        },
+    ];
+
+    get uncheckedTodos() {
+        return this.todos.filter(todo => !todo.checked);
+    }
+
+    get checkedTodos() {
+        return this.todos.filter(todo => todo.checked);
+    }
+
+    toggleAllTodos() {
+        this.todos
+            .filter((todo: TodoProp) => !todo.checked)
+            .map((todo: TodoProp) => todo.checked = true);
+    }
+
+    toggleTodo(todo: TodoProp) {
+        let todoIndex = this.todos.findIndex((innerTodo: TodoProp) => innerTodo.name === todo.name);
+        this.todos[todoIndex].checked = !todo.checked;
+    }
+
+    addNewTodo() {
+        this.todos.push({
+            name: this.name,
+            checked: false,
+        });
+
+        this.name = '';
+    }
+}
